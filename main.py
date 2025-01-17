ac15d34b0b519de2092f7d2f27af446522c1cb68
Br0ncosfan58 <mwasserstein2025@kehillahstudent.org>
2025-01-16 22:19:02 -0800

    rotation cipher

--- main.py
+++ main.py
@@ -27,3 +27,21 @@ print(encrypt(password))
 # Your task:
 # figure out what key I used to encrypt this message:
 secret_message = "y qc q iushuj cuiiqwu oek mybb duluh wkuii"
+
+def decrypt(ciphertext, key):
+    plaintext = ""
+    for letter in ciphertext:
+        old_position = alphabet.find(letter)
+        if old_position == -1:
+            plaintext += " "
+        else:
+            new_position = old_position - key
+            new_position = new_position % len(alphabet)
+            new_letter = alphabet[new_position]
+            plaintext += new_letter
+    return plaintext
+
+# finding the write key
+for k in range(len(alphabet)): # using the wrap-around
+    print(f"Key {k}: {decrypt(secret_message, k)}")
+
